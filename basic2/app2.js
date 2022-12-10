d6e5fa37fe3c47d39ffe5ca2234ed1138b9d1acd
admin01 <paradoxraphael@gmai.com>
2022-12-10 13:06:31 +1100

    node module system

--- basic2/app2.js
+++ basic2/app2.js
@@ -1,40 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/'){
-        res.write('<html>');
-        res.write('<head><title>Enter message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message" value="message"><input type="submit" value="Send"></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if (url ==='/message' && method === 'POST'){
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            console.log(parseBody);
-            const message = parseBody.split('=')[1]
-            fs.writeFileSync('message.txt', message);
-        });
-        
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<body><h1>Hello from my js server</h1></body>');
-    res.write('</html>');
-    res.end();
-    //process.exit();
-});
-
+const server = http.createServer(routes.handler);
+console.log(routes.someText);
 server.listen(3000);
