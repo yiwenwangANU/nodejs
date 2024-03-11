dbbd753daa97c91dd224f3143ad84eb9b050e9fe
admin01 <paradoxraphael@gmai.com>
2024-03-11 15:51:58 +1100

    add Cart model

---
+++ Section9_Dynamic_Routes/models/cart.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const fs = require('fs');
+const p = path.join(path.dirname(require.main.filename), 'data', 'cart.json');
+
+const getFromCart = cb => {
+    fs.readFile(p, (err, filecontent) => {
+        if(!err){
+            cb(JSON.parse(filecontent));
+        }
+        else{
+            cb({products:[],totalPrice:0})
+        }
+    })
+}
+
+module.exports = class Cart{
+    constructor(products, totalPrice){
+        this.products = products;
+        this.totalPrice = totalPrice;
+    }
+
+    static addToCart(id, price){
+        getFromCart(cartconent => {
+            const products = cartconent.products;
+            const totalPrice = cartconent.totalPrice;
+            let updatedProducts;
+            const index = products.findIndex(product => product.id === id);
+            if(index == -1){
+                updatedProducts = [...products, {id:id,qty:1}];
+            }
+            else{
+                updatedProducts = [...products];
+                updatedProducts[index].qty = updatedProducts[index].qty + 1;
+            }
+            const updatedCart = {updatedProducts, totalPrice: totalPrice + +price};
+            fs.writeFile(p, JSON.stringify(updatedCart), err => {
+                console.log(err);
+            })
+        })
+    }
+}
