53fb2c3fd24d0352c769a0522be506d04f78ea41
admin01 <paradoxraphael@gmai.com>
2024-03-28 18:30:46 +1100

    manage passwords

---
+++ Section12_MongoDB/utils/database.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb')
+
+let dbConnection;
+
+module.exports =  {
+    connectToDb: cb => {
+        MongoClient.connect('mongodb+srv://username:passw@cluster0.rb9vaxh.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
+        .then(client => {
+            dbConnection = client.db();
+            cb();
+        })
+        .catch(err => {
+            console.log(err);
+            return err
+        });
+    },
+    getDb: () => {
+        if(dbConnection){
+            return dbConnection
+        }
+        throw "No database found"
+    }
+}
