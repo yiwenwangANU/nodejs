60fa0d0ea8a10dcae820933147beee3579c55659
admin01 <paradoxraphael@gmai.com>
2024-05-15 10:10:04 +1000

    delete isAuthenticated in controllers

--- Section15_Authentication/controllers/auth.js
+++ Section15_Authentication/controllers/auth.js
@@ -6,7 +6,6 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: false,
     errorMessage: errorMessage.length > 0 ? errorMessage : null
   });
 };
@@ -16,7 +15,6 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    isAuthenticated: false,
     errorMessage: errorMessage.length > 0 ? errorMessage : null
   });
 };
